7fe1b3a8f844b6f324d4064137bafbf41681d5b5
egorlichenkoam <egorlichenkoam@gmail.com>
2022-04-11 20:52:28 +0500

    Main : задание без использования stream

--- src/ru/eam/Main.java
+++ src/ru/eam/Main.java
@@ -1,8 +1,31 @@
 package ru.eam;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Integer> integerList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> positiveEvenList = new ArrayList<>();
+        for (Integer x : integerList) {
+            if ((x > 0)
+                    && (x % 2 == 0)) {
+                positiveEvenList.add(x);
+            }
+        }
+        for (int i = 0; i < positiveEvenList.size(); i++) {
+            for (int j = positiveEvenList.size() - 1; j > i; j--) {
+                if (positiveEvenList.get(i) > positiveEvenList.get(j)) {
+                    int tmp = positiveEvenList.get(i);
+                    positiveEvenList.set(i,positiveEvenList.get(j));
+                    positiveEvenList.set(j,tmp);
+                }
+            }
+        }
+        for (Integer x : positiveEvenList) {
+            System.out.println(x);
+        }
     }
 }
